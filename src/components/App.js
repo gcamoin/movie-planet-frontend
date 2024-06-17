de35fe5480bbae4f6b4c31b9909edfc9b810e025
Garrett <gcamoin5@gmail.com>
2024-06-17 18:13:02 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Header from "/home/gcamoin/movie-planet-frontend/src/components/Header.js"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+
+    function Header() {
+        return(
+            <div className="header" align="center">
+                <h1>Movie Planet</h1>
+                <img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2023/11/greatest-movies-of-all-time.jpg?q=70&fit=crop&w=780&h=433&dpr=1" alt="logo" width="600"    />
+            </div>
+        )
+
+
+    }
+
+
+    export default Header;
